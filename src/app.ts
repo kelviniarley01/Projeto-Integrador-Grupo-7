5fb6b1317d9a38b1bba53ee92104d7df1b3387d3
Kelvin <kelviniarley6@gmail.com>
2026-05-06 19:40:18 -0300

    SRC pronto só falta testar o request.http

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,38 @@
 import express from 'express';
-import { FreteController } from "./controllers/Frete"
+
+import { UsuariosController } from "./controllers/usuarios";
+import { ProdutosController } from "./controllers/produtos";
+import { PedidosController } from "./controllers/pedido";
+import { PagamentosController } from "./controllers/pagamento";
+import { CarrinhosController } from "./controllers/carrinhos";
+import { ItensCarrinhosController } from "./controllers/itens_carrinhos";
+import { ItemPedidoController } from "./controllers/item_pedido";
+import { IngredientesController } from "./controllers/ingredientes";
+import { IngredientesProdutosController } from "./controllers/ingredientes_produtos";
+import { InformacoesProdutoController } from "./controllers/informações_produto";
+import { FreteController } from "./controllers/Frete";
+import { LojaController } from "./controllers/loja";
+import { AjudasController } from "./controllers/ajudas";
+import { AdministradorController } from "./controllers/administrador";
+
 
 export const app = express();
 app.use(express.json());
 
+UsuariosController();
+ProdutosController();
+PedidosController();
+PagamentosController();
+CarrinhosController();
+ItensCarrinhosController();
+ItemPedidoController();
+IngredientesController();
+IngredientesProdutosController();
+InformacoesProdutoController();
 FreteController();
+LojaController();
+AjudasController();
+AdministradorController();
 
 app.listen(3000, () => {
   console.log("Aplicação no ar 🚀");
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,77 +1,77 @@
 -----USUARIOS --------------
 CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_usuario TEXT,
-    email_usuario TEXT,
-    senha_usuario TEXT,
+    nome_usuario TEXT NOT NULL,
+    email_usuario TEXT UNIQUE NOT NULL,
+    senha_usuario TEXT NOT NULL,
     idade_usuario INTEGER,
     data_cadastro_usuario TEXT,
-    rua_usuario TEXT,
-    numero_usuario INTEGER,
-    cidade_usuario TEXT
-    estado_usuario TEXT
+    rua TEXT,
+    numero TEXT,
+    cidade TEXT,
+    estado TEXT
 );
 
 -----PRODUTOS --------------
 CREATE TABLE IF NOT EXISTS produtos (
     id_produto INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_produto TEXT,
+    nome_produto TEXT NOT NULL,
     descricao_produto TEXT,
-    preco_produto REAL,
-    estoque_produto INTEGER
+    preco_produto REAL NOT NULL,
+    quantidade_produto INTEGER
 );
 
 -----INGREDIENTES --------------
 CREATE TABLE IF NOT EXISTS ingredientes (
     id_ingrediente INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_ingrediente TEXT,
-    descricao_ingrediente TEXT
+    nome_ingrediente TEXT NOT NULL,
+    tipo_ingrediente TEXT
 );
 
 ----- INGREDIENTES_PRODUTOS --------------
 CREATE TABLE IF NOT EXISTS ingredientes_produtos (
     id_ingrediente_produto INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_produto INTEGER,
-    id_ingrediente INTEGER,
+    id_produto INTEGER NOT NULL,
+    id_ingrediente INTEGER NOT NULL,
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
     FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id_ingrediente)
 );
 
 ------CARRINHO -------
-CREATE TABLE IF NOT EXISTS carrinho (
+CREATE TABLE IF NOT EXISTS carrinhos (
     id_carrinho INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_usuario INTEGER,
-    preco_produto REAL,
+    id_usuario INTEGER NOT NULL,
+    preco_total REAL,
     valor_frete REAL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
 ----- ITEMS_CARRINHO --------------
-CREATE TABLE IF NOT EXISTS item_carrinho (
+CREATE TABLE IF NOT EXISTS itens_carrinho (
     id_item_carrinho INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_carrinho INTEGER,
-    id_produto INTEGER,
-    quantidade INTEGER,
-    FOREIGN KEY (id_carrinho) REFERENCES carrinho(id_carrinho),
+    id_carrinho INTEGER NOT NULL,
+    id_produto INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL,
+    FOREIGN KEY (id_carrinho) REFERENCES carrinhos(id_carrinho),
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
 );
 
 -----PEDIDOS --------------
 CREATE TABLE IF NOT EXISTS pedidos (
-    id_pedido INTEGER PRIMARY KEY AUTONICREMENT,
-    nome_usuario TEXT,
+    id_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_usuario INTEGER NOT NULL,
     data DATETIME,
     valor_total REAL,
-    status_pedido TEXT
-    FOREIGN KEY (nome_usuario) REFERENCES usuarios(nome_usuario)
+    status TEXT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
 -----ITEMS_PEDIDOS --------------
-CREATE TABLE IF NOT EXISTS item_pedido (
+CREATE TABLE IF NOT EXISTS itens_pedido (
     id_item_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_pedido INTEGER,
-    id_produto INTEGER,
-    quantidade INTEGER,
+    id_pedido INTEGER NOT NULL,
+    id_produto INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL,
     preco_unitario REAL,
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
@@ -79,8 +79,8 @@ CREATE TABLE IF NOT EXISTS item_pedido (
 
 ---- PAGAMENTOS --------------
 CREATE TABLE IF NOT EXISTS pagamentos (
-    id_pagamento INTEGER PRIMARY KEY AUTOICREMENT,
-    id_pedido INTEGER,
+    id_pagamento INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_pedido INTEGER NOT NULL,
     tipo_pagamento TEXT,
     status_pagamento TEXT,
     valor_pagamento REAL,
@@ -89,9 +89,9 @@ CREATE TABLE IF NOT EXISTS pagamentos (
 
 ----FRETES --------------
 CREATE TABLE IF NOT EXISTS fretes (
-    id_frete INTEGER  PRIMARY KEY AUTOINCREMENT,
-    id_pedido INTEGER,
-    valor_frete REAL,
+    id_frete INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_pedido INTEGER NOT NULL,
+    valor REAL,
     prazo_entrega TEXT,
     rua TEXT,
     numero INTEGER,
@@ -103,31 +103,31 @@ CREATE TABLE IF NOT EXISTS fretes (
 ----- ADMINISTRADORES --------------
 CREATE TABLE IF NOT EXISTS administradores (
     id_administrador INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_administrador TEXT,
-    email_administrador TEXT,
-    senha_administrador TEXT
+    nome_administrador TEXT NOT NULL,
+    email_administrador TEXT UNIQUE NOT NULL,
+    senha_administrador TEXT NOT NULL
 );
 
 --- AJUDAS --------------
 CREATE TABLE IF NOT EXISTS ajudas (
     id_pergunta INTEGER PRIMARY KEY AUTOINCREMENT,
-    pergunta TEXT,
+    pergunta TEXT NOT NULL,
     resposta TEXT
 );
 
 --- LOJA --------------
 CREATE TABLE IF NOT EXISTS loja (
-    id_loja INTEGER PRIMARY KEY AUTONICREMENT,
-    nome_loja TEXT,
-    descricao_loja TEXT,
+    id_loja INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_loja TEXT NOT NULL,
+    descricao_loja TEXT
 );
 
 ----INFORMAÇÕES_PRODUTO----------------------
 CREATE TABLE IF NOT EXISTS informacoes_produto (
     id_informacao INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_produto TEXT,
-    beneficios TEXT,
+    id_produto INTEGER NOT NULL,
+    beneficios_produto TEXT,
     modo_uso TEXT,
     conservacao TEXT,
-    FOREIGN KEY (nome_produto) REFERENCES produtos(nome_produto)
+    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
 );
